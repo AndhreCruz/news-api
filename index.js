c0a1543835fe4489aa84fcead9cec7a2052e1910
Andhre Cruz <138709296+AndhreCruz@users.noreply.github.com>
2024-12-02 16:56:35 -0300

    Fix server binding

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080; 
 const path = require('path');
 
 app.use(cors());
 
-app.get('/news', (req, res)=>{
-    fs.readFile(path.join(__dirname, 'newsData.json'),'utf8',(err, data)=> {
-        if(err) {
+app.get('/news', (req, res) => {
+    fs.readFile(path.join(__dirname, 'newsData.json'), 'utf8', (err, data) => {
+        if (err) {
             return res.status(500).json({ message: 'No se encontro ninguna noticia' });
         }
         res.json(JSON.parse(data));
     });
 });
 
-// Start the server
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`The server is running on http://0.0.0.0:${PORT}`);
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`The server is running on http://localhost:${PORT}`);
 });
